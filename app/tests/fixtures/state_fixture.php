5bea6f9a6c9dcb9cb66f42b62b13611929e777e0
zaininnari <zaininnari@gmail.com>
2010-06-22 23:01:03 +0900

    fix tests/Fix/state "engine" => "InnoDB"

--- app/tests/fixtures/state_fixture.php
+++ app/tests/fixtures/state_fixture.php
@@ -13,7 +13,7 @@ class StateFixture extends CakeTestFixture {
 		'created' => array('type' => 'datetime', 'null' => false, 'default' => NULL),
 		'modified' => array('type' => 'datetime', 'null' => false, 'default' => NULL),
 		'indexes' => array('PRIMARY' => array('column' => 'id', 'unique' => 1)),
-		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_unicode_ci', 'engine' => 'MyISAM')
+		'tableParameters' => array('charset' => 'utf8', 'collate' => 'utf8_unicode_ci', 'engine' => 'InnoDB')
 	);
 
 }
